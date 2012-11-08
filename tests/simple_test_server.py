bb22fbe2822f8a7344370bdc906ecdd2ea9c8011
bataille16 <bataille16@gmail.com>
2012-11-08 14:58:37 -0500

    fixed test server for send and recv

--- tests/simple_test_server.py
+++ tests/simple_test_server.py
@@ -4,7 +4,12 @@ import socket
 sock_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_fd.bind(('127.0.0.1', 12345))
 sock_fd.listen(1)
-sock_fd.accept()
+conn, addr = sock_fd.accept()
 
 while True:
-    time.sleep(1)
+	data = conn.recv(1024)
+	if not data:
+		break
+	conn.send(data)
+conn.close()
+    #time.sleep(1)
